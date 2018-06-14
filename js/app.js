159800e6fe9ef44fd6bb93f71ad39998d64b48f5
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-13 19:17:47 -0600

    Commit 2 - Get Even answered

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-for (let i = 1; i < 21; i++) {
-	console.log(i);
+// for (let i = 1; i < 21; i++) {
+// 	console.log(i);
+// }
+
+for (let i = 0; i < 201; i++) {
+	if(i % 2 == 0) {
+		console.log(i);
+	}
 }
