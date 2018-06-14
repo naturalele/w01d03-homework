f1a77549e0d490871bd7d86ad500edd6a5860027
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-13 20:17:42 -0600

    Commit 3 - Excited Kittens attempted and stuck on random

--- js/app.js
+++ js/app.js
@@ -2,8 +2,24 @@
 // 	console.log(i);
 // }
 
-for (let i = 0; i < 201; i++) {
-	if(i % 2 == 0) {
-		console.log(i);
+// for (let i = 0; i < 201; i++) {
+// 	if(i % 2 == 0) {
+// 		console.log(i);
+// 	}
+// }
+
+
+for (let i = 0; i < 21; i++) {
+	console.log("Love me, pet me! HSSSSSS!"); {
+		if(i % 2 == 0) { 
+			let phrases = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+		Math.random(phrases); {
+		console.log(phrases);
 	}
 }
+}
+}
+		
+	
+
+
