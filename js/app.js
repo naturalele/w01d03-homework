f98e36d728ab2a4dfa983b1ebf18f91a0c8a6c1b
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-13 20:34:31 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -9,17 +9,24 @@
 // }
 
 
-for (let i = 0; i < 21; i++) {
-	console.log("Love me, pet me! HSSSSSS!"); {
-		if(i % 2 == 0) { 
-			let phrases = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
-		Math.random(phrases); {
-		console.log(phrases);
-	}
-}
-}
-}
+// for (let i = 0; i < 21; i++) {
+// 	console.log("Love me, pet me! HSSSSSS!"); {
+// 		if(i % 2 == 0) { 
+// 			let phrases = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+// 		Math.random(phrases); {
+// 		console.log(phrases);
+// 	}
+// }
+// }
+// }
 		
-	
+
+for (let i = 1; i < 101; i ++) {
+	console.log(i);
+
+if((i % 3 == 0) && (i % 5 == 0)) {
+	console.log("Fizz");
+}
+}
 
 
